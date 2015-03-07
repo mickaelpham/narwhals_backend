ac59862170dad5f7572a5f3298862307e221de16
eric huang <erichuang310@gmail.com>
2015-03-06 23:48:06 -0800

    Fixed white spaces. Debugging session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
-	validates :password, presence: true
+  validates :password, presence: true
   validates :session_token, presence: true, uniqueness: true
   after_initialize :ensure_session_token
 
-	# User
-	def self.find_by_credentials(email, password)
+  # User
+  def self.find_by_credentials(email, password)
     user = User.find_by_email_and_password(email, password)
     user && user.is_password?(password) ? user : nil
   end
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
 
   # Capital One
   def has_capital_one_access_token?
-  	# TODO CHECK IF CAP ONE LOGIN IS OKAY
-  	true
+    # TODO CHECK IF CAP ONE LOGIN IS OKAY
+    true
   end
 end
