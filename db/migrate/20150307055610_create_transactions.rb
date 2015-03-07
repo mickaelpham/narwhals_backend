44c6c9ce21ff0cbdd5e42a10322f72851f502d5e
Eric Huang <zeneric@zendesk.com>
2015-03-07 14:19:25 -0800

    adding transaction amount column

--- db/migrate/20150307055610_create_transactions.rb
+++ db/migrate/20150307055610_create_transactions.rb
@@ -1,15 +1,16 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-    	t.integer  :user_id, null: false, index: true
-    	t.string   :co_transaction_id
-    	t.string   :co_account_id
+      t.integer  :user_id, null: false, index: true
+      t.string   :co_transaction_id
+      t.string   :co_account_id
       t.string   :merchant
       t.string   :raw_merchant
-    	t.string   :merchant
-    	t.boolean  :is_pending
-    	t.date     :transaction_time
-    	t.string   :categorization
+      t.string   :merchant
+      t.boolean  :is_pending
+      t.date     :transaction_time
+      t.string   :categorization
+      t.integer  :amount
 
       t.timestamps null: false
     end
