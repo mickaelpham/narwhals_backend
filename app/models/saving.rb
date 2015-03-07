36ac813006bda8dd46bcc32cf3388a8e7fcbd05f
Mickael Pham <mickael.pham@gmail.com>
2015-03-07 13:55:10 -0800

    Got the calculation right

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -1,28 +1,36 @@
 class Saving < ActiveRecord::Base
   belongs_to :co_transaction, class_name: 'Transaction'
 
-  private
+  COMPOUND_PER_YEAR = 12
+  INTEREST_RATE     = 0.10
+  NUM_YEAR          = 10
 
   def cost_projection
-
+    compound monthly_cost
   end
 
   def savings_projection
 
   end
 
+  private
+
   def cost
-    preview_cost || transaction.amount
+    self.respond_to?(:preview_cost) ? preview_cost : co_transaction.amount
   end
 
-  def yearly_cost
+  def monthly_cost
     case time_period
     when 'week'
-      frequency * 52 * cost
+      frequency * 52 / 12 * cost
     when 'month'
-      frequency * 12 * cost
-    when 'year'
       frequency * cost
+    when 'year'
+      frequency / 12 * cost
     end
+  end
+
+  def compound(monthly_payment)
+    monthly_payment * ((((1 + INTEREST_RATE / COMPOUND_PER_YEAR) ** (COMPOUND_PER_YEAR * NUM_YEAR)) - 1) / (INTEREST_RATE / COMPOUND_PER_YEAR))
   end
 end
