82203adeb42951a80aa1579ac02c6d7df50eedc2
Eric Huang <ehuang@zendesk.com>
2015-03-06 20:17:21 -0800

    Basic API with users

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,19 @@
+module V1
+  class UsersController < V1Controller
+    def create
+      user = User.new(user_params)
+
+      if user.save
+        render json: user, stats: :created
+      else
+        render json: { error: { message: user.errors.full_messages } }, stats: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+  end
+end
