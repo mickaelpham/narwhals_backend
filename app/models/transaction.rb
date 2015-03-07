376cf9fd537396cbfd5eed0ba4d35cb6a918ad3d
eric huang <erichuang310@gmail.com>
2015-03-07 10:33:28 -0800

    Added transaction methods

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
+  delegate :session, to: :user
+
+  def similar_transactions
+    session.similar_transactions([co_transaction_id]).map do |co_transaction_id|
+        Transaction.find_by_co_transaction_id(co_transaction_id)
+    end
+  end
 end
